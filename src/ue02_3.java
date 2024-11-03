a8af333c4adc9446498e936b9ca070670c1dabf5
Parmina D <parmina.darwish@fhooe.at>
2024-11-03 12:37:01 +0100

    ue2_3_fertig

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,26 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        
+        Out.print("Enter the x-coordinate of the point: ");
+        double x = In.readDouble();
+ 
+        Out.print("Enter the y-coordinate of the point: ");
+        double y = In.readDouble();
+ 
+    
+        if (x >= 0 && y >= 0) { 
+            double distanceToOrigin = Math.sqrt(x * x + y * y);
+            if (distanceToOrigin > 4) { 
+                Out.println("The point lies within the green area.");
+            } else {
+                Out.println("The point lies outside the green area.");
+            }
+        } else {
+            Out.println("The point lies outside the green area.");
+        }
+
     }
 }
