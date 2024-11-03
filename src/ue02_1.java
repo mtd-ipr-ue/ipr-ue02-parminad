66922e9afc82341de9ea28d9fbd8c629af492523
Parmina D <parmina.darwish@fhooe.at>
2024-11-03 11:29:59 +0100

    ue2_1_fertig

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,28 @@
 import libraries.Out;
+import libraries.In;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+
+        Out.print("Enter first number: ");
+        int firstNumber = In.readInt();
+
+        Out.print("Enter second number: ");
+        int secondNumber = In.readInt();
+
+        Out.print("Enter third number: ");
+        int thirdNumber = In.readInt();
+
+
+        int max = firstNumber;
+        if (secondNumber > max) {
+            max = secondNumber;
+        }
+        if (thirdNumber > max) {
+            max = thirdNumber;
+        }
+
+        Out.print("The maximum of your three numbers is: " + max);
+
     }
 }
